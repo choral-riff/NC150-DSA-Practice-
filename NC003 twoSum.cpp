1d837398e3c104afa72ea75c12b50a7c2f2271db
choral-riff <60531846+choral-riff@users.noreply.github.com>
2025-03-07 17:50:16 -0500

    Create NC003 twoSum.cpp

---
+++ NC003 twoSum.cpp
@@ -0,0 +1,53 @@
+/*
+Two Sum
+Solved 
+Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+Return the answer with the smaller index first.
+
+Example 1:
+
+Input: 
+nums = [3,4,5,6], target = 7
+
+Output: [0,1]
+Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
+
+Example 2:
+
+Input: nums = [4,5,6], target = 10
+
+Output: [0,2]
+Example 3:
+
+Input: nums = [5,5], target = 10
+
+Output: [0,1]
+Constraints:
+
+2 <= nums.length <= 1000
+-10,000,000 <= nums[i] <= 10,000,000
+-10,000,000 <= target <= 10,000,000
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> prevmap; //init a map to store index-value
+        int diff; //init diff before the for loop starts
+
+        
+        for (int i=0; i<nums.size(); i++){
+            diff = target - nums[i]; //calculating diff for each element of nums
+            if (prevmap.find(diff)!=prevmap.end()){
+                //if the difference is found in the map prevmap
+                return {prevmap[diff], i};
+                //return the index of difference and the index i 
+            }
+            prevmap.insert({nums[i],i});
+        }
+        return{};
+    }
+};
